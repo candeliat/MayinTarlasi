f6f28de778fa49fb5ac660e3507b5d7313753eae
Abdullah Can DELİAT <abdullahcandeliat1@gmail.com>
2024-11-24 09:59:19 +0300

    MayinTarlasi
    C# ile yapılmıştır.

---
+++ MayinTarlasi/MayinTarlasi/MayinTarlasi/Form2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayinTarlasi
+{
+    public partial class Form2 : Form
+    {
+        public int MineCount { get; private set; } 
+
+        private const int MaxMines = 900; 
+
+        public Form2()
+        {
+            InitializeComponent();
+            InitializeForm();
+        }
+
+        private void InitializeForm()
+        {
+            this.Text = "Mayın Sayısı Seçimi";
+            this.ClientSize = new Size(300, 150);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            Label label = new Label
+            {
+                Text = $"Lütfen Mayın Sayısını Girin (1 - {MaxMines}):",
+                Location = new Point(20, 20),
+                Size = new Size(250, 20)
+            };
+            this.Controls.Add(label);
+
+            TextBox textBox = new TextBox
+            {
+                Location = new Point(20, 50),
+                Size = new Size(250, 20),
+                Name = "MineInputBox"
+            };
+            this.Controls.Add(textBox);
+
+            Button button = new Button
+            {
+                Text = "Başlat",
+                Location = new Point(20, 90),
+                Size = new Size(250, 30)
+            };
+            button.Click += (sender, e) => StartGame(textBox);
+            this.Controls.Add(button);
+        }
+
+        private void StartGame(TextBox textBox)
+        {
+            if (int.TryParse(textBox.Text, out int mineCount) && mineCount > 0 && mineCount <= MaxMines)
+            {
+                MineCount = mineCount;
+                this.DialogResult = DialogResult.OK; 
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show($"Lütfen 1 ile {MaxMines} arasında geçerli bir sayı girin!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+    
+}
